15f32de00c10413131d6763d3f05f897852ca93c
Cesar Vasquez <heellxz@gmail.com>
2017-09-01 15:06:28 -0300

    prueba de divs en vez de table

---
+++ motto.js
@@ -0,0 +1,17 @@
+
+	var timer = setInterval(refresh, 5000);
+
+	var motto = [
+	"una criptomoneda bien bonita", 
+	"le chauché proyecté",
+	"dejale caer to' el peso",
+	"no se me ocurre que escribir",
+	"por la razón o la chaucha",
+	"aqui no se cae el sistema",
+	"jonny 100 chauchas"
+	];
+
+	function refresh () {
+		var rnd = Math.floor(Math.random()*motto.length);
+		$("#motto").html(motto[rnd]);
+	}
