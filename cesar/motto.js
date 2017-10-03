f9c24a6599e420a727e8f3cfc623d990b4874801
Cesar Vasquez <heellxz@gmail.com>
2017-10-03 01:10:19 -0300

    footer y main columns

---
+++ cesar/motto.js
@@ -0,0 +1,23 @@
+
+	var timer = setInterval(refresh, 5000);
+
+	var motto = [
+	"Una criptomoneda bien bonita", 
+	"Le chauché proyecté",
+	"dejale caer to' el peso",
+	"Por la razón o la chaucha",
+	"Aquí no se cae el sistema",
+	"リサフランク420 / 現代のコンピュー",
+	"Johnny 100 Chauchas"
+	];
+
+	function refresh () {
+		var rnd = Math.floor(Math.random()*motto.length);
+		var rnd_past = -1;
+
+		if(rnd != rnd_past) {
+			$("#motto").html(motto[rnd]);
+			rnd_past = rnd;
+		}
+		
+	}
